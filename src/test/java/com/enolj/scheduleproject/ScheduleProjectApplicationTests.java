de0415f120476fa0db414ca7d80177caa27bd7c7
eNoLJ <derosatam76@gmail.com>
2025-12-30 13:48:44 +0900

    init commit

---
+++ src/test/java/com/enolj/scheduleproject/ScheduleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enolj.scheduleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
